2e631e899ea8bba060c591756242e5cd8c63312d
Dylanon Wicchiramala <dylanonwic@gmail.com>
2024-07-02 10:27:36 +0700

    change database path

--- app/database/data.py
+++ app/database/data.py
@@ -1,12 +1,17 @@
 import os
 import sqlite3
 import logging
+from sys import platform
 logger = logging.getLogger(__name__)
 
-PATH = os.environ.get("DATA_PATH", "/var/lib/data")
-# PATH = os.environ.get("DATA_PATH", "./data")
+if platform == "linux" or platform == "linux2":
+    PATH = os.environ.get("DATA_PATH", "/var/lib/data")
+    DATABASE_PATH = "/var/lib/data/"+"financedata.db"
+else:
+    PATH = os.environ.get("DATA_PATH", "./data")
+
+    DATABASE_PATH = os.path.join(os.path.abspath(PATH), 'financedata.db')
+
+
 logger.warning("Data path location: {}".format(PATH))
-
-DATABASE_PATH = os.path.join(os.path.abspath(PATH), 'financedata.db')
-
 connection = sqlite3.connect(DATABASE_PATH, check_same_thread=False)
